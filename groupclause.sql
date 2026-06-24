3eaa24f97dc93421ee088762cb06dfd5600e868b
Sadikh Shaik <sadikhshaik7866@gmail.com>
2026-06-24 16:23:41 +0530

    Update groupclause.sql

--- groupclause.sql
+++ groupclause.sql
@@ -1,3 +1,4 @@
+#group function1
 create database school;
 use school;
 CREATE TABLE student (
@@ -13,3 +14,31 @@ INSERT INTO student VALUES ( 3,"hidayath","bangalore",10,92);
 iNSERT INTO student VALUES ( 4,"imran","hyderabad",21,90);
 select id from student;
 select city,count(name) from student group by city;
+
+
+#group function2
+create database school;
+use school;
+
+CREATE TABLE payments (
+   customer_id int primary key,
+   customer VARCHAR(50),
+   mode varchar(50),
+   city varchar(50)
+);
+insert into payments
+(customer_id,customer,mode,city )
+values
+(1,"nizam","netbanking","usa"),
+(2,"sadikh","debitcard","pak"),
+(3,"xyz","netbanking","uk"),
+(4,"hidaytath","debitcard","india"),
+(5,"imran","creditcard","israil"),
+(6,"qbc","netbanking","usa"),
+(7,"jkm","debitcard","usa");
+select * from payments;
+select mode,count(customer) from payments group by mode;
+select customer,mode from payments group by customer,mode;
+select * from payments where mode="netbanking" and "debitcard";
+select city,mode from payments group by city,mode;
+
