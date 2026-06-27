476c2d955d0ec2b7b5e9eee885d034fc2d06d062
Sadikh Shaik <sadikhshaik7866@gmail.com>
2026-06-27 19:02:31 +0530

    Update join.sql

--- join.sql
+++ join.sql
@@ -58,3 +58,17 @@ select *
 from  student as s
 right join course as c
 on s.id=c.cid;
+
+#Exclusive right join
+select *
+from student
+right join course
+on student.id=course.cid
+where student.id is null;
+
+#Exclusive left join
+select *
+from student
+left join course
+on student.id=course.cid
+where course.cid is null;
